a250f1c3a2446fe1984226c74da07053a696f515
Bekzat Baktybekov <bekzatbaktybekov4@gmail.com>
2022-07-15 09:14:44 +0600

    fix ignoreArr

--- repositories/postRepository.js
+++ repositories/postRepository.js
@@ -1,68 +1,76 @@
 class PostRepository {
-    async getAll(page = 1, limit = 10, ignoreArr = []) {
-        const ingoreStr = ignoreArr.reduce((curr, next) => curr + '&id_ne=' + next, '')
+	async getAll(page = 1, limit = 10, ignoreArr = []) {
+		const ingoreStr =
+			ignoreArr && ignoreArr.length > 1
+				? ignoreArr.reduce((curr, next) => curr + "&id_ne=" + next, "")
+				: "";
 
-        const res = await fetch(
-            `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}&_expand=user&${ingoreStr}`,
-        )
-        const posts = await res.json()
+		const res = await fetch(
+			`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}&_expand=user&${ingoreStr}`
+		);
+		const posts = await res.json();
 
-        return posts
-    }
+		return posts;
+	}
 
-    async getById(id) {
-        const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}?_expand=user`)
-        const post = res.json()
+	async getById(id) {
+		const res = await fetch(
+			`https://jsonplaceholder.typicode.com/posts/${id}?_expand=user`
+		);
+		const post = res.json();
 
-        return post
-    }
+		return post;
+	}
 
-    async deleteOne(id) {
-        const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + id, {
-            method: 'DELETE',
-        })
-        const deletedPost = await res.json()
+	async deleteOne(id) {
+		const res = await fetch(
+			"https://jsonplaceholder.typicode.com/posts/" + id,
+			{
+				method: "DELETE",
+			}
+		);
+		const deletedPost = await res.json();
 
-        return deletedPost
-    }
+		return deletedPost;
+	}
 
-    async search(text, field) {
-        if (text === '') {
-            return await this.getAll(1)
-        }
+	async search(text, field) {
+		if (text === "") {
+			return await this.getAll(1);
+		}
 
-        if (field === 'author') {
-            return await this.searchByAuthor(text)
-        }
+		if (field === "author") {
+			return await this.searchByAuthor(text);
+		}
 
-        const res = await fetch(
-            `https://jsonplaceholder.typicode.com/posts?${field}_like=${text}&_expand=user`,
-        )
-        const posts = await res.json()
+		const res = await fetch(
+			`https://jsonplaceholder.typicode.com/posts?${field}_like=${text}&_expand=user`
+		);
+		const posts = await res.json();
 
-        return posts
-    }
+		return posts;
+	}
 
-    async searchByAuthor(name) {
-        if (name === '') {
-            return await this.getAll(1)
-        }
+	async searchByAuthor(name) {
+		if (name === "") {
+			return await this.getAll(1);
+		}
 
-        const res = await fetch(
-            `https://jsonplaceholder.typicode.com/users?username=${name}&_embed=posts`,
-        )
-        const user = await res.json()
-        const fixedPosts = await user[0].posts.map((post) => {
-            return {
-                ...post,
-                user: {
-                    username: user[0].username,
-                },
-            }
-        })
+		const res = await fetch(
+			`https://jsonplaceholder.typicode.com/users?username=${name}&_embed=posts`
+		);
+		const user = await res.json();
+		const fixedPosts = await user[0].posts.map((post) => {
+			return {
+				...post,
+				user: {
+					username: user[0].username,
+				},
+			};
+		});
 
-        return fixedPosts
-    }
+		return fixedPosts;
+	}
 }
 
-export const postRepository = new PostRepository()
+export const postRepository = new PostRepository();
